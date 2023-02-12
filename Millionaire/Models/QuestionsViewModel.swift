c6e951d963683ee7538c0c73e40e963c30b9b9ee
Jigg1y <abhazjiggly@gmail.com>
2023-02-12 13:06:35 +0300

    added button for questions

--- Millionaire/Models/QuestionsViewModel.swift
+++ Millionaire/Models/QuestionsViewModel.swift
@@ -4,45 +4,63 @@
 //
 //  Created by Сергей Золотухин on 05.02.2023.
 //
-
-//struct QuestionsViewModel {
-    import UIKit
-
-    class YellowGradientButton: UIButton {
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            setupButton()
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            setupButton()
-        }
-        
-        private func setupButton() {
-            let yellowColor = UIColor.yellow
-            let yellowMiddleColor = UIColor(red: 1, green: 1, blue: 0, alpha: 0.5)
-            let yellowGradient = CAGradientLayer()
-            yellowGradient.colors = [yellowColor.cgColor, yellowMiddleColor.cgColor, yellowColor.cgColor]
-            yellowGradient.startPoint = CGPoint(x: 0, y: 0)
-            yellowGradient.endPoint = CGPoint(x: 1, y: 1)
-            yellowGradient.frame = bounds
-            layer.insertSublayer(yellowGradient, at: 0)
-        }
-    }
-class ViewController: UIViewController {
+import UIKit
+struct QuestionsViewModel {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 44))
-        button.backgroundColor = .yellow
-        button.setTitle("Button", for: .normal)
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        view.addSubview(button)
+
+    private let bottomView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+        class YellowGradientButton: UIButton {
+            
+            override init(frame: CGRect) {
+                super.init(frame: frame)
+                setupButton()
+            }
+            
+            required init?(coder aDecoder: NSCoder) {
+                super.init(coder: aDecoder)
+                setupButton()
+            }
+            
+            private func setupButton() {
+                let yellowColor = UIColor.yellow
+                let yellowMiddleColor = UIColor(red: 1, green: 1, blue: 0, alpha: 0.5)
+                let yellowGradient = CAGradientLayer()
+                yellowGradient.colors = [yellowMiddleColor.cgColor, yellowColor.cgColor, yellowMiddleColor.cgColor]
+                yellowGradient.startPoint = CGPoint(x: 0, y: 0)
+                yellowGradient.endPoint = CGPoint(x: 1, y: 1)
+                yellowGradient.frame = bounds
+                layer.insertSublayer(yellowGradient, at: 0)
+            }
+        }
+    class ViewController: UIViewController {
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let button = UIButton(frame: CGRect(x: 100, y: 100, width: 50, height: 40))
+            button.backgroundColor = .yellow
+            button.setTitle("Button", for: .normal)
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            view.addSubview(button)
+        }
+        
+        @objc func buttonTapped(_ sender: UIButton) {
+            print("Button Tapped")
+        }
     }
-    
-    @objc func buttonTapped(_ sender: UIButton) {
-        print("Button Tapped")
-    }
+
+    private let questionNumberLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "question 5"
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        return label
+    }()
 }
